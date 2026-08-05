b674b518df3a9cebd2b821a8350cbfc829fc69c3
BasAdrean <Adreanoliveirabastos@live.com>
2026-08-05 20:55:34 -0300

    Finishing chapter 2

--- src/cap2/Main.java
+++ src/cap2/Main.java
@@ -24,6 +24,35 @@ public class Main {
         ep.skipIntro();
         ep.play();*/
 
+        /*DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        d.playTopHat();
+
+        if(d.snare){
+        d.playSnare();
+        }*/
+
+        Echo e1 = new Echo();
+        //Challenge
+
+        //e2 = new Echo();
+
+        Echo e2 = e1;
+
+        int x = 0;
+        while( x <4 ){
+            e1.hello();
+            e1.count = e1.count + 1;
+            if(x==3){
+                e2.count = e2.count + 1;
+            }
+            if(x>0){
+                e2.count = e2.count + e1.count;
+            }
+            x = x + 1;
+        }
+        System.out.println(e2.count);
     }}
 class Player{
     int number;
@@ -110,3 +139,23 @@ class episode{
         System.out.println("Playing episode " + episodeNumber + " from the serie " + seriesName);
     }
 }
+
+class Echo {
+    int count = 0;
+    void hello(){
+        System.out.println("Heloooo... ");
+    }
+}
+
+class DrumKit{
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare(){
+        System.out.println("Bang bang ba-bang");
+    }
+
+    void playTopHat(){
+        System.out.println("ding ding da-ding");
+    }}
